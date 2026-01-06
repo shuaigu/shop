ca2880aeada861b3d8aba29375956f07747e6954
李帅007 <1020556299@qq.com>
2026-01-06 18:48:36 +0800

    c

--- 帅帅预约/unpackage/dist/dev/mp-kuaishou/common/assets.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const _imports_0 = "/static/images/logo.png";
-const _imports_1 = "/static/images/auth-icon.png";
-exports._imports_0 = _imports_0;
-exports._imports_1 = _imports_1;
--- 帅帅预约/unpackage/dist/dev/mp-kuaishou/uni_modules/uview-plus/libs/mixin/mpMixin.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const uni_modules_uviewPlus_libs_vue = require("../vue.js");
-const mpMixin = uni_modules_uviewPlus_libs_vue.defineMixin({});
-exports.mpMixin = mpMixin;
